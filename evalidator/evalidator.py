4189859270d6fc7622dc7de49f9a10e635ed08c2
ritiek <ritiekmalhotra123@gmail.com>
2017-08-24 13:30:01 +0530

    Improve readability

--- evalidator/evalidator.py
+++ evalidator/evalidator.py
@@ -1,22 +1,35 @@
 import requests
 import sys
 import json
+import argparse
 
 
 email = 'someone@email.com'
 
-if email.endswith('@gmail.com'):
+def google_mail(email):
     data = {'Email': email}
     response = requests.post('https://accounts.google.com/_/signin/v1/lookup', data=data)
     text = json.loads(response.text)
     is_valid = text['action'] == 'ASK_PASSWORD'
-else:
-    print('This e-mail service is not supported currently. Please submit an issue on https://github.com/ritiek/email-validator/issues and thank you!')
-    sys.exit(1)
+    return is_valid
 
-if is_valid:
-    result = email + ' is a valid e-mail address'
-else:
-    result = email + ' is an invalid e-mail address'
 
-print(result)
+def valididate(email):
+    if email.endswith('@gmail.com'):
+        is_valid = google_mail(email)
+    else:
+        raise()
+    return is_valid
+
+
+def command_line():
+    is_valid = validate(email)
+
+    if is_valid:
+        result = email + ' is an invalid e-mail address'
+    elif is_valid:
+        result = email + ' is a valid e-mail address'
+    else:
+        result = 'This e-mail service is not supported currently. Please submit an issue on https://github.com/ritiek/email-validator/issues and thank you!'
+
+    print(result)
