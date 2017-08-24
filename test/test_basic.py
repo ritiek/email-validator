df24df4a620bb4737438b55d1122cfc011ea5622
ritiek <ritiekmalhotra123@gmail.com>
2017-08-24 19:36:48 +0530

    Add Yahoo! mail test

--- test/test_basic.py
+++ test/test_basic.py
@@ -5,3 +5,9 @@ def test_googlemail():
     valid = evalidator.validate('someone@gmail.com')
     invalid = evalidator.validate('thisemaildoesnotexisthehe@gmail.com')
     assert (valid and not invalid)
+
+
+def test_yahoomail():
+    valid = evalidator.validate('hello@yahoo.com')
+    invalid = evalidator.validate('thisemaildoesnotexisthehe@yahoo.com')
+    assert (valid and not invalid)
